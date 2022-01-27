fcf77047e2aa3e63be505964d73cab7c58133991
taurusboong2 <taurusboong2@gmail.com>
2022-01-27 19:04:24 +0900

    @5 basic principle and Link

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>홈 page</h1>
+      <p>이 페이지는 홈입니다. 가장 먼저 보여지는 페이지랍니다.</p>
+    </div>
+  );
+};
+
+export default Home;
