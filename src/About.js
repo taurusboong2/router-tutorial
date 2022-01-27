e82fa74d8db82ecd6aea43ce04c095d434170f15
taurusboong2 <taurusboong2@gmail.com>
2022-01-27 19:21:25 +0900

    get query & qs library

--- src/About.js
+++ src/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  const detail = query.detail === 'true';
+  // 쿼리의 파싱결과값은 문자열입니다.
+
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 라우터를 사용해보는 프로젝트입니다.</p>
+      {detail && <p>추가적인 정보가 어쩌고 저쩌고..</p>}
     </div>
   );
 };
